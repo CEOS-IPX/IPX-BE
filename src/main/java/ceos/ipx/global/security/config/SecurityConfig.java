9e78a7582c4c6139ccb2851d25f08654b394e22c
taeik <kti1452@naver.com>
2026-06-28 16:57:22 +0900

    feat: IPX 도메인 CORS 설정 추가

--- src/main/java/ceos/ipx/global/security/config/SecurityConfig.java
+++ src/main/java/ceos/ipx/global/security/config/SecurityConfig.java
@@ -79,8 +79,9 @@ public class SecurityConfig {
 
         // 허용 도메인
         config.setAllowedOrigins(List.of(
-                "http://localhost:3000"            // 프론트 개발 환경
-//                "https://?????.com"          // 운영 도메인
+                "http://localhost:3000",            // 프론트 개발 환경
+                "https://ipx-patent.com",          // 운영 도메인
+                "https://www.ipx-patent.com"          // 운영 도메인
         ));
 
         // 허용 HTTP 메서드

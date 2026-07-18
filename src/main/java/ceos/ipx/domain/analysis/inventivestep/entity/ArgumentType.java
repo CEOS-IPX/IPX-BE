92f1102cacce3b4258f114001e8bed2f72dea7f0
Oh-Jisong <dhwlthd4725@gmail.com>
2026-07-18 14:56:47 +0900

    feat: 분석 리포트 상세 조회 API 구현

--- src/main/java/ceos/ipx/domain/analysis/inventivestep/entity/ArgumentType.java
+++ src/main/java/ceos/ipx/domain/analysis/inventivestep/entity/ArgumentType.java
@@ -1,8 +1,18 @@
 package ceos.ipx.domain.analysis.inventivestep.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum ArgumentType {
-    NUMERICAL_LIMIT,            // 수치한정 - 효과의 현저성
-    COMBINATION_MOTIVATION,     // 복수인용발명결합 - Teaching Away
-    COMMON_TECHNIQUE,           // 주지관용기술 - 반박 논리
-    SIMPLE_DESIGN               // 단순설계변경 - 비자명성 논리
+
+    NUMERICAL_LIMIT("수치한정"),
+    COMBINATION_MOTIVATION("복수인용발명결합"),
+    COMMON_TECHNIQUE("주지관용기술"),
+    SIMPLE_DESIGN("단순설계변경");
+
+    private final String label;
+
+    ArgumentType(String label) {
+        this.label = label;
+    }
 }

d7e468f9daca0c63999b00e964595030b811dcff
Oh-Jisong <dhwlthd4725@gmail.com>
2026-07-18 10:59:40 +0900

    feat: 분석 리포트 생성 및 덮어쓰기 API 구현

--- src/main/java/ceos/ipx/domain/report/repository/ReportRepository.java
+++ src/main/java/ceos/ipx/domain/report/repository/ReportRepository.java
@@ -1,12 +1,17 @@
 package ceos.ipx.domain.report.repository;
 
+import ceos.ipx.domain.cases.entity.Case;
 import ceos.ipx.domain.report.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    Optional<Report> findByCaseEntity(Case caseEntity);
 
     @Modifying
     @Query("DELETE FROM Report r WHERE r.caseEntity.id = :caseId")
